7b113563d7109164dbadd07d7a70b016815d7b31
nexlab-agents <agents@nexlab.dev>
2026-05-30 16:19:50 -0500

    Lazy load video tab embeds

--- resources/views/partials/video-card.blade.php
+++ resources/views/partials/video-card.blade.php
@@ -1,11 +1,23 @@
 <article class="video-card">
     <div class="video-thumb">
-        <iframe
-            src="https://www.youtube.com/embed/{{ $video['id'] }}"
-            title="Reny Renteria - {!! strip_tags($video['title']) !!}"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen>
-        </iframe>
+        <button
+            class="video-load-button"
+            type="button"
+            data-youtube-id="{{ $video['id'] }}"
+            data-youtube-title="Reny Renteria - {{ e(strip_tags(html_entity_decode($video['title']))) }}"
+            aria-label="Play Reny Renteria - {{ e(strip_tags(html_entity_decode($video['title']))) }}"
+        >
+            <img
+                src="https://i.ytimg.com/vi/{{ $video['id'] }}/hqdefault.jpg"
+                alt=""
+                loading="lazy"
+                decoding="async"
+            >
+            <span class="video-play-icon" aria-hidden="true"></span>
+        </button>
+        <noscript>
+            <a href="https://www.youtube.com/watch?v={{ $video['id'] }}" target="_blank" rel="noreferrer">Watch on YouTube</a>
+        </noscript>
     </div>
     <h4>{!! $video['title'] !!}</h4>
     <p>{!! $video['meta'] !!}</p>
